09e500568f63c3e49aed1f54b76a4c07918c71b0
Jorge Amestoy Sabucedo <jamestoysabucedo@gmail.com>
2022-10-14 10:26:00 +0200

    version 2.0

--- src/boletin4/Boletin4.java
+++ src/boletin4/Boletin4.java
@@ -7,7 +7,7 @@ public class Boletin4 {
    
     public static void main(String[] args) {
 
-        Libros obxLib1 = new Libros("IT", "Stephen King", 1978, (short) 892);
+        Libros obxLib1 = new Libros("IT", "Stephen King", 1968, (short) 892);
         //por defecto es int y quiero short asi que lo casteo
         obxLib1.mostrar();
 
